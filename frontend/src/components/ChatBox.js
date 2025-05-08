10726a476bc9b62b5ce1934ff397d6794471f679
hefnyy <mohammedahefny1111@gmail.com>
2025-05-08 18:37:01 +0300

    fix

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -9,20 +9,20 @@ const Chatbox = ({ fetchAgain, setFetchAgain }) => {
 
     return (
         <>
+        <Box
+            display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+            alignItems="center"
+            flexDir="column"
+            p={3}
+            bg="white"
+            w={{ base: "100%", md: "68%" }}
+            borderRadius="lg"
+            borderWidth="1px"
+        >
+            {/* <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} /> */}
         Single Chat
+        </Box>
         </>
-        // <Box
-        //     display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
-        //     alignItems="center"
-        //     flexDir="column"
-        //     p={3}
-        //     bg="white"
-        //     w={{ base: "100%", md: "68%" }}
-        //     borderRadius="lg"
-        //     borderWidth="1px"
-        // >
-        //     {/* <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} /> */}
-        // </Box>
     );
 };
 
